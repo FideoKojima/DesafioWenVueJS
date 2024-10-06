a7209ea4fad75b52a2d58ae07d14a1540711f12f
Luis Suarez <luisdcobain@gmail.com>
2024-10-05 22:36:46 -0300

    Add initial project files and configurations

---
+++ src/components/carnet.vue
@@ -0,0 +1,13 @@
+<script>
+export default{
+    name: 'carnet',
+    data(){
+        return{
+            carnet: '22.222.222-2',
+            apellidos: 'MUSK', 
+            nombres: 'ELON REEVE',
+            nacionalidad: 'SUDAFRICANO', 
+            sexo: 'M',
+            fechaNacimiento: '28/06/1971',
+            numeroDocumento: '22.222.222-2',
+            }</script>
